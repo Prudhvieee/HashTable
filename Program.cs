2c13b105a772af1ad19bfc63d8b4e01b9aaf1eda
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-09 22:13:42 +0530

    [Prudhvi] Add . Finding frequency of words in a sentence

--- Program.cs
+++ Program.cs
@@ -7,6 +7,16 @@ namespace HashTable
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table implementation");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "To");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("4");
+            Console.WriteLine("5th index value:" + hash5);
         }
     }
 }
